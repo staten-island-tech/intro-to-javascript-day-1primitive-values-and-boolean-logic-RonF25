bccb645f318c8f2c0b9e1c5957ad70086c9de18a
RonF25 <57496986+RonF25@users.noreply.github.com>
2020-03-24 15:51:02 -0400

    gd

--- notes/index.js
+++ notes/index.js
@@ -1,1 +1,56 @@
 console.log("Connected");
+
+/*let string  = "string";
+const edis = "Edis";
+let sentence = "Hello" + "Vicky";
+let sentence2 = " and also Angel";
+let combine = sentence + sentence2;
+//console.log(combine);
+const combine = '${sentence}${sentence2}';*/
+//let string = "strings";
+//let sentence = "Hello" + "Junshen";
+//let sentence2 = " and Ema";
+//console.log(sentence + sentence2);
+//"url"+"movieName"
+//let student1 = "Sandra";
+//let student2 = "Bob";
+/*const longersentence =
+    student1 +
+    "and" +
+    student2*/
+ /*const longSentence = `${student1} and ${student2}
+ are amazing and will be taking ${courseName} with 
+ ${teachername}`; //template literal or template string
+
+ let url= ${url}${movie}*/
+ //////////Numbers or Integers  
+/*let year = 2020; //"2020" is a string
+let nextYear = 2020 + 1;
+let past = 2020 / 2;
+let farFuture = 2020 * 50;*/
+//////Booleans
+/*let trueStatement = true;
+let falseStatement = false;*/
+///Null and Undefined
+//let Ron; //declared undefined but not defined undefined
+//console.log(Ron);
+//console.log(Sarah);
+//////Boolean Logic
+//let student = "Caroline";
+//let age = 15;
+//console.log(student === "Caroline")
+//console.log(student === "caroline")
+//console. log(age === 15);
+//console.log(age <= 15 || age > 19); // || means or
+// &&=both
+///////If statements
+let school = "Middle";
+if(school === "Elementary"){
+    console.log("You are in k-5")
+} else if (school === "Middle"){
+    console.log("You are in 6,7, or 8th grade");
+} else {
+    console.log("your are in HS");
+}
+let ron= "Ron";
+console.log(ron !== "Ema");//!== not equal
